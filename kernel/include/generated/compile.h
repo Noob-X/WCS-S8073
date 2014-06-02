e29c4bd6eaa029a8ae958d8849c2704fab564b7c
linux <linux@andLinux.(none)>
2014-06-02 11:36:09 -0400

    Initial Noob Commit
     Please enter the commit message for your changes. Lines starting

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 63 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#63 SMP PREEMPT Tue May 27 14:27:30 EDT 2014"
+#define LINUX_COMPILE_BY "FaithFreedom"
+#define LINUX_COMPILE_HOST "Noob_XDA"
+#define LINUX_COMPILER "gcc version 4.6.3 (Sourcery CodeBench Lite 2012.03-57) "
---
+++ mediatek/build/tools/patch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./mkimage zImage KERNEL > kernel
