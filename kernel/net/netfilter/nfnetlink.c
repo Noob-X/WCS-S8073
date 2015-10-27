0c1a13fe1cacabc92f5bc3e8af5e500cd1c4ed1a
Florian Westphal <fw@strlen.de>
2015-10-27 13:20:25 +0100

    netfilter: nfnetlink: don't probe module if it exists
    commit dbc3617f4c1f9fcbe63612048cb9583fea1e11ab upstream.
    
    nfnetlink_bind request_module()s all the time as nfnetlink_get_subsys()
    shifts the argument by 8 to obtain the subsys id.
    
    So using type instead of type << 8 always returns NULL.
    
    Fixes: 03292745b02d11 ("netlink: add nlk->netlink_bind hook for module auto-loading")
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Luis Henriques <luis.henriques@canonical.com>

--- kernel/net/netfilter/nfnetlink.c
+++ kernel/net/netfilter/nfnetlink.c
@@ -221,7 +221,7 @@ static void nfnetlink_bind(int group)
 	int type = nfnl_group2type[group];
 
 	rcu_read_lock();
-	ss = nfnetlink_get_subsys(type);
+	ss = nfnetlink_get_subsys(type << 8);
 	rcu_read_unlock();
 	if (!ss)
 		request_module("nfnetlink-subsys-%d", type);
