8d25a999643fa256b699f26df14c01b08d6f2467
Jan Kara <jack@suse.cz>
2016-05-26 16:55:18 +0200

    fs: Give dentry to inode_change_ok() instead of inode
    commit 31051c85b5e2aaaf6315f74c72a732673632a905 upstream.
    
    inode_change_ok() will be resposible for clearing capabilities and IMA
    extended attributes and as such will need dentry. Give it as an argument
    to inode_change_ok() instead of an inode. Also rename inode_change_ok()
    to setattr_prepare() to better relect that it does also some
    modifications in addition to checks.
    
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jan Kara <jack@suse.cz>
    [bwh: Backported to 3.2:
     - Drop changes to f2fs, lustre, orangefs, overlayfs
     - Adjust filenames, context
     - In nfsd, pass dentry to nfsd_sanitize_attrs()
     - In xfs, pass dentry to xfs_change_file_space(), xfs_set_mode(),
       xfs_setattr_nonsize(), and xfs_setattr_size()
     - Update ext3 as well
     - Mark pohmelfs as BROKEN; it's long dead upstream]
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- kernel/fs/cifs/inode.c
+++ kernel/fs/cifs/inode.c
@@ -1978,7 +1978,7 @@ cifs_setattr_unix(struct dentry *direntry, struct iattr *attrs)
 	if (cifs_sb->mnt_cifs_flags & CIFS_MOUNT_NO_PERM)
 		attrs->ia_valid |= ATTR_FORCE;
 
-	rc = inode_change_ok(inode, attrs);
+	rc = setattr_prepare(direntry, attrs);
 	if (rc < 0)
 		goto out;
 
@@ -2117,7 +2117,7 @@ cifs_setattr_nounix(struct dentry *direntry, struct iattr *attrs)
 	if (cifs_sb->mnt_cifs_flags & CIFS_MOUNT_NO_PERM)
 		attrs->ia_valid |= ATTR_FORCE;
 
-	rc = inode_change_ok(inode, attrs);
+	rc = setattr_prepare(direntry, attrs);
 	if (rc < 0) {
 		FreeXid(xid);
 		return rc;
